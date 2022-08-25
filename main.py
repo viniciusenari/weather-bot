6c1572da6f081ea5e7f252f71c6860924e88c0f5
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-08-25 14:59:35 -0300

    refactor: Get current weather, receive parameters
    Instead of using the weather from 3 hours ahead as the first timestep, use the current weather. Set latitude and longitude and units as method parameters.

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     twitter = Twitter()
     twitter.authenticate()
     weather = WeatherAPI()
-    weather.get_data()
+    weather.get_data(lat = "-23.55", lon = "-46.64", units="metric")
     tweet = weather.create_tweet()
     twitter.post_tweet(tweet)
--- project/weather.py
+++ project/weather.py
@@ -1,15 +1,26 @@
 import requests
-from project.config import weather_api_key
-from datetime import datetime, time
-from project.utils import is_time_between, create_icon, format_day
+from config import weather_api_key
+from datetime import datetime, time, timedelta
+from utils import is_time_between, create_icon, format_day
 
 class WeatherAPI:
     def __init__(self):
         self.forecast = []
 
-    def get_data(self):
-        response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?lat=-23.55&lon=-46.64&appid={weather_api_key}&units=metric')
-        for item in response.json()['list'][:7]:
+    def get_data(self, lat, lon, units = 'metric'):
+        # Get current weather
+        response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_api_key}&units={units}')
+        datetime_object = datetime.now() + timedelta(hours=0.5)
+        day = True if is_time_between(time(6,0), time(18,1), datetime_object.time()) else False
+        self.forecast.append({
+            'temperature' : (float(response.json()['main']['temp_min']) + float(response.json()['main']['temp_min'])) / 2,
+            'icon' : create_icon(response.json()['weather'][0]['id'], True),
+            'datetime' : datetime_object
+        })
+
+        # Get following steps
+        response = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={weather_api_key}&units={units}')
+        for item in response.json()['list'][:6]:
             datetime_object = datetime.strptime(item['dt_txt'], "%Y-%m-%d %H:%M:%S")
             day = True if is_time_between(time(6,0), time(18,1), datetime_object.time()) else False
             self.forecast.append({
